6658af4a5d9533c5a079aa1232075777675b94e2
fred-15 <fredafriyieatitso@gmail.com>
2024-06-14 00:01:40 +0000

    Slider ripple effect added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Use Strict Mode (Optional)
+"use strict";
+
+// jQuery Document Ready Shorthand
+$(function(){
+    $('.slider').ripples({
+        dropRadius: 7,
+        perturbance: 0.01,
+        interactive: true
+
+    });
+});
+
